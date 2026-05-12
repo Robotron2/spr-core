61fdb4b3d03ca9a9353596edcc5a0b3220b77c28
Theophilus <theophilusadesola002@gmail.com>
2026-05-12 13:42:25 +0100

    ci: configure GitHub Actions and resolve ESLint/build pipeline issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    es2022: true,
-  },
-  extends: ["eslint:recommended"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  ignorePatterns: ["node_modules/", "dist/", "build/"],
-  rules: {},
-};
+	root: true,
+	env: {
+		node: true,
+		es2022: true,
+	},
+	extends: ["eslint:recommended"],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	ignorePatterns: ["node_modules/", "dist/", "build/"],
+	rules: { "no-unused-vars": ["error", { argsIgnorePattern: "^_" }] },
+	overrides: [
+		{
+			files: ["**/*.test.js"],
+			env: {
+				jest: true,
+			},
+		},
+	],
+}
